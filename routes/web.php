31fbe2f91a8477c73289388c78d2747e89cc029d
Efo <supertomaroma@gmail.com>
2021-04-22 16:15:21 +0000

    Fixed Auth Routes

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
 
 Auth::routes();
-
-//Login Routes
-Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
-Route::post('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-
-//Registration Routes
-Route::get('register', [App\Http\Controllers\Auth\LoginController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [App\Http\Controllers\Auth\LoginController::class, 'register']);
-
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
